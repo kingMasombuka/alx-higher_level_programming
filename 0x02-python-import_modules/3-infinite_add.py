9bbbba6e9f8f0bdcfe10776d761c02d083754c56
Senzo Ncongwane <senzoncongwane21@gmail.com>
2023-08-12 00:15:46 +0200

    ghg

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,18 +1,12 @@
-
-
-import sys
-from itertools import islice
-
-
-def execute_code():
-    argv = sys.argv
-    argcount = len(argv) - 1
-    if argcount == 0:
-        print(argcount)
-    else:
-        for i, arg in islice(enumerate(argv), 1, None):
-            print("{}: {}".format(i, arg))
+##!/usr/bin/python3
 
 
 if __name__ == "__main__":
-    execute_code()
+    import sys
+
+    sum = 0
+    argv = sys.argv
+    argcount = len(argv) - 1
+    for arg in argv[1:]:
+        sum += int(arg)
+    print(sum)
