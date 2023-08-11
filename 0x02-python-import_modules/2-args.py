78e964604d8d786a5f7f7a4835e161947f2084e3
Senzo Ncongwane <senzoncongwane21@gmail.com>
2023-08-12 00:03:57 +0200

    gf

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 
-import sys
-from itertools import islice
-
-
-def execute_code():
+if __name__ == "__main__":
+    import sys
+    from itertools import islice
     argv = sys.argv
     argcount = len(argv) - 1
     if argcount == 0:
@@ -16,9 +14,3 @@ def execute_code():
         print("{} arguments:".format(argcount))
         for i, arg in islice(enumerate(argv), 1, None):
             print("{}: {}".format(i, arg))
-
-
-if __name__ == "__main__":
-    execute_code()
-    
-    
