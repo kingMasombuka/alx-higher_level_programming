832dc93a1b4c4be96a3ca80d750f083e5cb0ddc0
Senzo Ncongwane <senzoncongwane21@gmail.com>
2023-08-12 09:44:28 +0200

    hot fix

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,7 +6,25 @@ if __name__ == "__main__":
 
     argv = sys.argv
     argcount = len(argv) - 1
+    operators = ["+", "*", "/", "-"]
     if argcount != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    print(argv)
-    print(argcount)
+        sys.exit(1)
+    if argv[2] not in operators:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+    try:
+        a = int(argv[1])
+        b = int(argv[3])
+    except ValueError:
+        print("Input Numbers only like : 1 ,2 ,3 ,4")
+        sys.exit(1)
+    if argv[2] == "+":
+        print("{} {} {} = {}".format(a, argv[2], b, cal.add(a, b)))
+    if argv[2] == "-":
+        print("{} {} {} = {}".format(a, argv[2], b, cal.sub(a, b)))
+    if argv[2] == "*":
+        print("{} {} {} = {}".format(a, argv[2], b, cal.mul(a, b)))
+    if argv[2] == "/":
+        print("{} {} {} = {}".format(a, argv[2], b, cal.div(a, b)))
+    sys.exit(0)
