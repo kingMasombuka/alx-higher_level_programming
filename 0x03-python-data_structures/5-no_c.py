76792b307dbe45e5525e6a320139563da874f4a0
Senzo Ncongwane <senzoncongwane21@gmail.com>
2023-08-12 20:39:39 +0200

    initial commit

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+##!/usr/bin/python3
+
+def no_c(my_string):
+    to_list = list(my_string)
+    new_list = [str(x) for x in to_list if x != "c" and x != "C" ]
+    my_1string = ''.join(new_list)
+    return my_1string
