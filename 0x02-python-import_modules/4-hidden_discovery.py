c05cf8d94baa62bc106a18d3626860784b11ab24
Senzo Ncongwane <senzoncongwane21@gmail.com>
2023-08-12 08:17:31 +0200

    hot fix

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    
+
     import hidden_4 as hid
 
-    names = dir(hid)
-    for name in names:
+    lines = dir(hid)
+    for name in lines:
         if name[:2] != "__":
             print(name)
