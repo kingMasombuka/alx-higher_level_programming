fde3262ce69968ffa5ba20c3d707d1599eb62d3e
Senzo Ncongwane <senzoncongwane21@gmail.com>
2023-08-12 08:15:10 +0200

    ddd

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    print
+    
+    import hidden_4 as hid
+
+    names = dir(hid)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
