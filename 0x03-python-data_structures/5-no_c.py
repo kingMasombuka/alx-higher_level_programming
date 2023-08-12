9178e06546f56296064f9173c08494860dbda351
Senzo Ncongwane <senzoncongwane21@gmail.com>
2023-08-12 20:42:05 +0200

    hot fix

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 
 def no_c(my_string):
     to_list = list(my_string)
-    new_list = [str(x) for x in to_list if x != "c" and x != "C" ]
+    new_list = [str(x) for x in to_list if x != "c" and x != "C"]
     my_1string = ''.join(new_list)
     return my_1string
