d9e81cbfaa0888af9e2766385c6210da3bd90414
Senzo Ncongwane <senzoncongwane21@gmail.com>
2023-09-10 18:22:43 +0200

    fix code

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -71,7 +71,8 @@ class Rectangle:
 
         rectangle_d = []
         for i in range(self.__height):
-            [rectangle_d.append(str(self.print_symbol)) for j in range(self.__width)]
+            [rectangle_d.append(str(self.print_symbol)) 
+                for j in range(self.__width)]
             if i != self.__height - 1:
                 rectangle_d.append("\n")
         return ("".join(rectangle_d))
